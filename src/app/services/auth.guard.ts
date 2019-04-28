a957423d88da60481a0561aa4526d509a0b3412d
Ben <taihuynh@Bens-MacBook-Air.local>
2019-04-28 13:37:05 +1000

    successful implement the auth guard for security route

---
+++ src/app/services/auth.guard.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { ToastController } from '@ionic/angular';
+import { Observable } from 'rxjs';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate  {
+  constructor(
+    private router: Router,
+    private afAuth:AngularFireAuth,
+    private toastCtrl:ToastController
+  ){}
+  canActivate(): boolean | Observable<boolean> | Promise<boolean> {
+    return new Promise((resolve, reject) => {
+      this.afAuth.auth.onAuthStateChanged(user => { //check user state persist
+        if (user) {//if current user, allow to go to the other page
+          resolve(true);
+        } 
+        else {//if not ,navigate back to the login until sign in
+          this.router.navigate(['/register']);
+          this.showToast("Please sign in first");
+          reject('No Logged in');
+        }
+      }); 
+    });
+  }
+
+  async showToast(message){
+    const toast = await this.toastCtrl.create({
+      message: message,
+      duration: 2000
+    });
+    toast.present();
+  }
+}
